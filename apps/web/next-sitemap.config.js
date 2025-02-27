6f685253d4594326f616ca17b0af6d42ff3cb32a
Michael Smith <msmith@slideheroes.com>
2025-02-27 15:38:32 -0500

    finetuning production setup

---
+++ apps/web/next-sitemap.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    'https://2025slideheroes-web.vercel.app',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  outDir: './public',
+};
