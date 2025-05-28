ce04705818386cb977e5b8b17ed1463f82b5159a
Michael Smith <msmith@slideheroes.com>
2025-05-28 16:09:51 -0400

    migration reset

--- apps/payload/src/migrations/index.ts
+++ apps/payload/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250528_190106___name from './20250528_190106___name';
+import * as migration_20250528_200440___name from './20250528_200440___name';
 
 export const migrations = [
   {
-    up: migration_20250528_190106___name.up,
-    down: migration_20250528_190106___name.down,
-    name: '20250528_190106___name'
+    up: migration_20250528_200440___name.up,
+    down: migration_20250528_200440___name.down,
+    name: '20250528_200440___name'
   },
 ];
