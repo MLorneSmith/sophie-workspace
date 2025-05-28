96bd13ff7dd9be41aedc91e1d099acfb9cebeb47
Michael Smith <msmith@slideheroes.com>
2025-05-28 15:17:19 -0400

    S3 changes to config

--- apps/payload/src/collections/Media.ts
+++ apps/payload/src/collections/Media.ts
@@ -5,12 +5,33 @@ export const Media: CollectionConfig = {
   access: {
     read: () => true,
   },
-  upload: true,
+  admin: {
+    useAsTitle: 'filename',
+  },
+  upload: {
+    // Make sure there are no custom ID configurations here
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+    ],
+  },
   fields: [
+    // Make sure there's no custom 'id' field defined here
     {
       name: 'alt',
       type: 'text',
-      required: false,
+      required: true,
     },
+    // Other fields...
   ],
 }
